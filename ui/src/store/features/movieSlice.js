f932cbdd3f63e0157c2afa7e63e137b2beed93bf
hallokay <hallokay044@gmail.com>
2023-06-01 15:32:27 +0900

    리덕스 slice 코드 개선

--- ui/src/store/features/movieSlice.js
+++ ui/src/store/features/movieSlice.js
@@ -1,67 +1,84 @@
-import { async } from "@firebase/util";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import {TMDB_BASE_URL, API_KEY} from '../../util/constants'
-
+import generateApiUrl from "../../util/generateApiUrl";
 
 const initialState = {
-    movies: [],
-    genresLoded: false,
-    genres: []
-}
+  movies: [],
+  genresLoaded: false,
+  genres: [],
+};
 
+export const getGenres = createAsyncThunk("movie/genres", async () => {
+  const url = generateApiUrl("/genre/movie/list");
+  
+  const {
+    data: { genres },
+  } = await axios.get(url);
+  console.log("genres", genres);
+  return genres;
+});
 
-
-//영화의 장르의 목록을 가져온다.
-export const getGenres = createAsyncThunk('movie/genres', async() => {
-  const { data: {genres} } = await axios.get(`${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`);
-    return genres
-})
-
-// 인기 있는 영화 데이터를 가져옴 fetch
-//type으로 영화의 유형을 정할 수 잇고
-//thunkAPI 매개변수를 통해 현재 Redux 상태를 액세스할 수 있습니다. thunkAPI.getState()를 통해 현재 상태에서 netflix.genres를 가져옴
-export const fetchMovies = createAsyncThunk('movie/trending', async({type}, thunkApi) => {
+export const fetchMovies = createAsyncThunk(
+  "movie/trending",
+  async ({ type }, thunkApi) => {
     const {
-        movie: {genres},
+      movie: { genres },
     } = thunkApi.getState();
-    return getRawData(
-      `${TMDB_BASE_URL}/trending/${type}/week?api_key=${API_KEY}`,
-      genres,
-      true
-    );
-})
+    const apiUrl = generateApiUrl(`trending/${type}/week`);
+    const movies = await getRawData(apiUrl, genres);
+console.log('movies',movies);
+    return movies;
+  }
+);
 
-const getRawData = async (api, genres, paging = false) =>{
-    const moviesArr = [];
-    for(let i = 1; moviesArr.length < 60 && i < 10; i++) {
-        const {
-            data: {results}
-        } = await axios.get(`${api}${paging ? `&page=${i}` : ''}`);
-        createArrFromRawData(results, moviesArr, genres);
-    }
-
-}
+const getRawData = async (api, genres) => {
+  const moviesArr = [];
+  for (let page = 1; moviesArr.length < 60 && page < 10; page++) {
+    const {
+      data: { results },
+    } = await axios.get(api, { params: { page } });
+    createArrFromRawData(results, moviesArr, genres);
+  }
+  return moviesArr;
+};
+ 
 
 const createArrFromRawData = (resultsArr, moviesArr, genres) => {
-resultsArr.forEach((movie) => {
+  resultsArr.forEach((movie) => {
     const movieGenres = [];
-    
-})
-}
+    movie.genre_ids.forEach((genre) => {
+      const name = genres.find(({ id }) => id === genre);
+      if (name) movieGenres.push(name.name);
+    });
+
+    if (movie.backdrop_path) {
+      moviesArr.push({
+        id: movie.id,
+        name: movie?.original_name
+          ? movie?.original_name
+          : movie.original_title,
+        image: movie.backdrop_path,
+        genres: movieGenres.slice(0, 3),
+      });
+    }
+  });
+};
+
 const movieSlice = createSlice({
-    name: 'movie',
-    initialState,
-    extraReducers: (builder) => {
-        builder.addCase(getGenres.fulfilled, (state, action) => {
-          state.genres = action.payload;
-          state.genresLoded = true;
-        }),
-          builder.addCase(fetchMovies.fulfilled, (state, action) => {
-            state.movies = action.payload;
-          
-          });
-    },
-})
+  name: "movie",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getGenres.fulfilled, (state, action) => {
+      // console.log(action.payload);
+      state.genres = action.payload;
+      state.genresLoaded = true;
+    });
+    builder.addCase(fetchMovies.fulfilled, (state, action) => {
+      console.log('fetchMovies', action.payload);
+      
+      state.movies = action.payload;
+    });
+  },
+});
 
 export default movieSlice.reducer;
