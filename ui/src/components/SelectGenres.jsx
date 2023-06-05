6d8a210b2acac31996f0114ee8a4ae8a0a821e05
hallokay <hallokay044@gmail.com>
2023-06-05 22:14:05 +0900

    node.js ...어려워.... mongoDB에서 막힘

--- ui/src/components/SelectGenres.jsx
+++ ui/src/components/SelectGenres.jsx
@@ -16,5 +16,11 @@ const SelectGenres = ({ type }) => {
     </Select>
   );
 };
-const Select = styled.select``;
+const Select = styled.select`
+  margin-left: 5rem;
+  cursor: pointer;
+  font-size: 1.4rem;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: white;
+`;
 export default SelectGenres;
